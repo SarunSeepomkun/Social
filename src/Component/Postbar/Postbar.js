3e36ad1e66d5676b8ee2a583278715bb5a62795a
Sarun Seepomkun <sarunseepomkun@gmail.com>
2021-08-13 16:07:45 +0700

    Update 13-08-2021 4:06pm
    - Align content profile
    - Update Postbar

--- src/Component/Postbar/Postbar.js
+++ src/Component/Postbar/Postbar.js
@@ -1,29 +1,30 @@
-import React,{ createRef, useContext , useState } from "react";
+import React, { createRef, useContext, useState } from "react";
 import { AuthContext } from "../../Context/AuthContext";
 import { CreatePost } from "../../API/PostAPI";
 
 const Postbar = () => {
   const { user } = useContext(AuthContext);
   const messageRef = createRef();
-  const [loading,setLoading] = useState(false);
-  const [txtMessage,setTxtMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [txtMessage, setTxtMessage] = useState("");
 
   const NewPost_Click = async (e) => {
     setLoading(true);
     e.preventDefault();
     try {
-     const result = await CreatePost({
-        userID: user.userID,
-        message: messageRef.current.value,
-        token: user.token
-      });
-      if(result.statusText === "Created"){
-        setTxtMessage("");
+      if (messageRef.current.value) {
+        const result = await CreatePost({
+          userID: user.userID,
+          message: messageRef.current.value,
+          token: user.token,
+        });
+        if (result.statusText === "Created") {
+          setTxtMessage("");
+        }
       }
     } catch (error) {
       console.log(`Postbar.NewPost_Click,${error}`);
-    }
-    finally{
+    } finally {
       setLoading(false);
     }
   };
@@ -42,8 +43,8 @@ const Postbar = () => {
   };
 
   return (
-      <form onSubmit={NewPost_Click} autoComplete="off">
-        <div className="input-group mb-3">
+    <form onSubmit={NewPost_Click} autoComplete="off">
+      <div className="input-group mb-3">
         <input
           type="text"
           className="form-control"
@@ -52,17 +53,22 @@ const Postbar = () => {
           aria-describedby="btn-post"
           ref={messageRef}
           value={txtMessage}
-          onChange={(e)=> setTxtMessage(e.target.value)}
+          onChange={(e) => setTxtMessage(e.target.value)}
           maxLength="500"
+          required
         />
-        {loading === false ? <button
-          className="btn btn-outline-secondary"
-          type="submit"
-          id="btn-post"
-        >
-          Post
-        </button> : <LoadingButton />}
-    </div>
+        {loading === false ? (
+          <button
+            className="btn btn-outline-secondary"
+            type="submit"
+            id="btn-post"
+          >
+            Post
+          </button>
+        ) : (
+          <LoadingButton />
+        )}
+      </div>
     </form>
   );
 };
--- src/Component/Profile/Profile.js
+++ src/Component/Profile/Profile.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState, useContext, createRef } from "react";
-import { GetProfile } from "../../API/UserAPI";
+import { GetProfile, EditProfile } from "../../API/UserAPI";
 import { useParams } from "react-router-dom";
 import { AuthContext } from "../../Context/AuthContext";
 import { default as Loading } from "../Skeletons/Feeds/Feeds";
+import { Avatar } from "@material-ui/core";
 
 const Profile = () => {
   const userID_Param = useParams().userid;
   const [Profile, SetProfile] = useState({});
-  const user = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
+  const [loading, setLoading] = useState(false);
   const BioRef = createRef();
 
   useEffect(() => {
@@ -21,14 +23,53 @@ const Profile = () => {
     fectUserProfile();
   }, [userID_Param]);
 
+  const SaveProfile = async () => {
+    try {
+      setLoading(true);
+      const data = { token: user.token , userID:'', bio:'', country:'' };
+      const result = await EditProfile(data);
+      if (result) {
+      } else {
+      }
+    } catch (error) {
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const LoadingButton = () => {
+    return (
+      <button className="btn btn-outline-secondary" type="button" disabled>
+        <span
+          className="spinner-border spinner-border-sm"
+          role="status"
+          aria-hidden="true"
+        ></span>
+        Saving
+      </button>
+    );
+  };
+
   return (
-    <>
+    <div className="d-flex justify-content-center mt-5">
       {Profile ? (
-        <div className="container p-1 m-1">
+        <div className="container card p-1 m-1">
           <div className="row p-1 m-1">
-            <label htmlFor="txtUsername" className="col-sm-2 col-form-label">
-              Username
-            </label>
+            <div className="col-12">
+              <div className="d-flex flex-column justify-content-center align-items-center">
+                <button className="btn btn-link">
+                  <Avatar alt="avatar" src="" />
+                </button>
+                <label>Follower 0 Following 0</label>
+              </div>
+            </div>
+          </div>
+          <div className="row p-1 m-1">
+            <div className="col-sm-2">
+              <label htmlFor="txtUsername" className="col-form-label">
+                Username
+              </label>
+            </div>
             <div className="col-sm-10">
               {user.userID === Profile.userID && user ? (
                 <input
@@ -43,47 +84,62 @@ const Profile = () => {
               )}
             </div>
           </div>
-          <div className="row p-1 m-1">
-            <label htmlFor="txtPassword" className="col-sm-2 col-form-label">
-              Password
-            </label>
-            <div className="col-sm-10">
-              <input
-                type="password"
-                className="form-control"
-                id="txtPassword"
-                required
-              />
+
+          {user.userID === userID_Param ? (
+            <div className="row p-1 m-1">
+              <div className="col-sm-2">
+                <label htmlFor="txtPassword" className="form-label">
+                  Password
+                </label>
+              </div>
+              <div className="col-sm-10">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="txtPassword"
+                  required
+                />
+              </div>
             </div>
-          </div>
+          ) : (
+            ""
+          )}
 
           <div className="row p-1 m-1">
-            <label htmlFor="txtAddress" className="col-sm-2 col-form-label">
-              Gender
-            </label>
+            <div className="col-sm-2">
+              <label htmlFor="txtAddress" className="form-label">
+                Gender
+              </label>
+            </div>
             <div className="col-sm-10 col-form-check">
-              <input
-                id="radMale"
-                className="form-check-input mx-1"
-                type="radio"
-                value="male"
-                name="gender"
-                required
-              />
-              <label className="form-check-label mx-1" htmlFor="radMale">
-                Male
-              </label>
-              <input
-                id="radFemale"
-                className="form-check-input mx-1"
-                type="radio"
-                value="female"
-                name="gender"
-                required
-              />
-              <label className="form-check-label mx-1" htmlFor="radFemale">
-                Female
-              </label>
+              {user.userID === userID_Param ? (
+                <div>
+                  <input
+                    id="radMale"
+                    className="form-check-input mx-1"
+                    type="radio"
+                    value="male"
+                    name="gender"
+                    required
+                  />
+                  <label className="form-check-label mx-1" htmlFor="radMale">
+                    Male
+                  </label>
+                  <input
+                    id="radFemale"
+                    className="form-check-input mx-1"
+                    type="radio"
+                    value="female"
+                    name="gender"
+                    required
+                  />
+                  <label className="form-check-label mx-1" htmlFor="radFemale">
+                    Female
+                  </label>
+                </div>
+              ) : (
+                ""
+              )}
             </div>
           </div>
 
@@ -92,7 +148,7 @@ const Profile = () => {
               Email
             </label>
             <div className="col-sm-10">
-              {user.userID === Profile.userID ? (
+              {user.userID === userID_Param ? (
                 <input
                   type="email"
                   className="form-control"
@@ -109,7 +165,11 @@ const Profile = () => {
               Address
             </label>
             <div className="col-sm-10">
-              <input type="text" className="form-control" id="txtAddress" />
+              {user.userID === userID_Param ? (
+                <input type="text" className="form-control" id="txtAddress" />
+              ) : (
+                ""
+              )}
             </div>
           </div>
           <div className="row p-1 m-1">
@@ -117,7 +177,7 @@ const Profile = () => {
               Your Bio
             </label>
             <div className="col-sm-10">
-              {user.userID === Profile.userID && user.userID !== null ? (
+              {user.userID === userID_Param && user.userID !== null ? (
                 <input
                   type="textarea"
                   className="form-control"
@@ -132,20 +192,26 @@ const Profile = () => {
           </div>
           <div className="row p-1 m-1">
             <div className="col-xs-12">
-              {user.userID === Profile.userID ? (
-                <button type="submit" className="btn btn-primary">
-                  Save
-                </button>
-              ) : (
-                ""
-              )}
+              <div className="d-flex justify-content-center">
+                {user.userID === userID_Param ? (
+                  <button
+                    type="submit"
+                    className="btn btn-outline-primary"
+                    onClick={() => SaveProfile()}
+                  >
+                    Save
+                  </button>
+                ) : (
+                  ""
+                )}
+              </div>
             </div>
           </div>
         </div>
       ) : (
         <Loading />
       )}
-    </>
+    </div>
   );
 };
 
